23757857074d4b646df5fc103c4eb38c5c5ca193
Terence Parr <parrt@antlr.org>
2016-01-10 15:49:32 -0800

    add test for real sample from ST4 lib. Wow. seems to predict well with existing feature set.

--- play/random_forest_on_token_features.py
+++ play/random_forest_on_token_features.py
@@ -1,5 +1,3 @@
-# looking at https://www.kaggle.com/c/titanic/details/getting-started-with-random-forests
-
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 
@@ -7,7 +5,7 @@ forest = RandomForestClassifier(n_estimators = 100)
 
 # let's get some data. So called "iris" data
 # http://www.math.uah.edu/stat/data/Fisher.csv
-data = np.loadtxt("iris.csv", delimiter=",", skiprows=1)
+data = np.loadtxt("../samples/stringtemplate4/features.csv", delimiter=",", skiprows=1)
 
 X = data[0::,1::]	# features
 Y = data[0::,0]	    # prediction class
@@ -15,7 +13,7 @@ Y = data[0::,0]	    # prediction class
 # get first 70% as training data, 30% as testing data
 # it's 150 rows with 4 features, 1 class column (first col)
 n = len(data)
-last_training_index = n * 0.7
+last_training_index = n * 0.95
 X_training = X[0:last_training_index]
 X_testing = X[last_training_index:]
 Y_training = Y[0:last_training_index]
@@ -34,4 +32,6 @@ print Y_testing
 # print "diff"
 # print Y_testing-Y_predictions
 
-print "number misclassified:", int(abs(sum(Y_testing-Y_predictions)))
+misclassified = int(abs(sum(Y_testing - Y_predictions)))
+print "number misclassified: %d/%d=%f%%" %\
+      (misclassified, len(Y_testing), misclassified/float(len(Y_testing))*100)
