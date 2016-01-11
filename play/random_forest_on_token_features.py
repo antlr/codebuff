71ae69a7180214dc44fffc8a9f207afe1d2fbd24
Terence Parr <parrt@antlr.org>
2016-01-10 16:58:33 -0800

    add more comments, clean up. more output.

--- play/random_forest_on_token_features.py
+++ play/random_forest_on_token_features.py
@@ -1,33 +1,77 @@
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
+import matplotlib.pyplot as plt
 
-forest = RandomForestClassifier(n_estimators = 100)
+def print_importance(forest):
+    importances = forest.feature_importances_
+    indices = np.argsort(importances)[::-1]
+    # Compute stddev of forest's feature importances over all trees
+    std = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
+    for f in range(X_training.shape[1]):
+        print "%d. feature %s col %d (%f, inter-tree variability=%f)" % \
+              (f + 1,
+               features[indices[f]],
+               indices[f],
+               importances[indices[f]],
+               std[indices[f]])
+
+def graph_importance(forest):
+    importances = forest.feature_importances_
+    std = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
+    indices = np.argsort(importances)[::-1]
+
+    fig, ax = plt.subplots(1,1)
+    plt.title("Feature importances")
+    xlabels = [features[int(i)] for i in indices]
+    plt.bar(range(X_training.shape[1]), importances[indices],
+           color="r", yerr=std[indices], align="center")
+    plt.xticks(range(X_training.shape[1]), xlabels, rotation=15)
+    plt.xlim([-1, X_training.shape[1]])
+    plt.ylim([0, 1])
+
+    for tick in ax.xaxis.get_major_ticks():
+        tick.tick1line.set_markersize(0)
+        tick.tick2line.set_markersize(0)
+        tick.label1.set_horizontalalignment('right')
+
+    plt.show()
 
 # let's get some data. So called "iris" data
 # http://www.math.uah.edu/stat/data/Fisher.csv
 data = np.loadtxt("../samples/stringtemplate4/features.csv", delimiter=",", skiprows=1)
 
+features = []
+with open("../samples/stringtemplate4/features.csv", 'r') as f:
+    features = f.readline().strip().split(', ')
+    features = features[1:] # first col is predictor var
+
 X = data[0::,1::]	# features
 Y = data[0::,0]	    # prediction class
+
+print "a priori 'inject newline' is %d/%d = %f" % (sum(Y), len(Y), sum(Y)/float(len(Y)))
 
 # get first 70% as training data, 30% as testing data
 # it's 150 rows with 4 features, 1 class column (first col)
 n = len(data)
-last_training_index = n * 0.95
+last_training_index = n * 0.60
 X_training = X[0:last_training_index]
 X_testing = X[last_training_index:]
 Y_training = Y[0:last_training_index]
 Y_testing = Y[last_training_index:]
 
+forest = RandomForestClassifier(n_estimators = 600)
 forest = forest.fit(X_training, Y_training)
 
 Y_predictions = forest.predict(X_testing)
 
-print "predictions:"
-print Y_predictions
+print "prediction 'inject newline' rate is %d/%d = %f" % \
+      (sum(Y_predictions), len(Y_predictions), sum(Y_predictions)/float(len(Y_predictions)))
 
-print "actual:"
-print Y_testing
+# print "predictions:"
+# print Y_predictions
+#
+# print "actual:"
+# print Y_testing
 
 # print "diff"
 # print Y_testing-Y_predictions
@@ -35,3 +79,9 @@ print Y_testing
 misclassified = int(abs(sum(Y_testing - Y_predictions)))
 print "number misclassified: %d/%d=%f%%" %\
       (misclassified, len(Y_testing), misclassified/float(len(Y_testing))*100)
+
+print_importance(forest)
+
+# now graph and show important features
+
+graph_importance(forest)
