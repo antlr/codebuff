0bcd947413e7d1b6c402eb59c90fbe4f0522c857
Terence Parr <parrt@antlr.org>
2016-01-16 12:54:50 -0800

    add java test program

---
+++ java/src/org/antlr/groom/ProfileJava.java
@@ -0,0 +1,17 @@
+package org.antlr.groom;
+
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+public class ProfileJava {
+	public static void main(String[] args) throws Exception {
+		Thread.sleep(10000);
+		ANTLRFileStream input = new ANTLRFileStream(args[0]);
+		JavaLexer lexer = new JavaLexer(input);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		JavaParser parser = new JavaParser(tokens);
+		JavaParser.CompilationUnitContext tree = parser.compilationUnit();
+//		System.out.println(tree.toStringTree(parser));
+		Thread.sleep(10000);
+	}
+}
