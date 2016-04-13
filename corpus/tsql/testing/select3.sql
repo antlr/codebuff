8d887379fb0db8995fac93472ba4d743436957b9
parrt <parrt@cs.usfca.edu>
2016-04-13 15:51:42 -0700

    add new test.

---
+++ corpus/tsql/testing/select3.sql
@@ -0,0 +1,8 @@
+-- this is also in training set
+SELECT DISTINCT Name
+FROM Production.Product AS p 
+WHERE EXISTS
+    (SELECT *
+     FROM Production.ProductModel AS pm 
+     WHERE p.ProductModelID = pm.ProductModelID
+           AND pm.Name LIKE 'Long-Sleeve Logo Jersey%');
