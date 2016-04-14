96f0bf1af235ec6e33d713142ee7c5a35f61734c
parrt <parrt@cs.usfca.edu>
2016-04-14 09:36:33 -0700

    add test file

---
+++ corpus/java/training/Align.java
@@ -0,0 +1,23 @@
+class Align {
+	int a = { a,
+			  f,
+			  c
+			};
+	
+	int x;
+
+	@Override
+	public static void f(int i, int z, float foo) {
+		x = 1+
+			2+
+			3;
+	}
+
+	public static Corpus processSampleDocs(List<InputDocument> docs,
+										   int tabSize,
+										   Map<String, List<Pair<Integer, Integer>>> ruleToPairsBag)
+		throws Exception
+	{
+		List<InputDocument> documents = new ArrayList<>();
+	}
+}
