e08d4cde9fbc99a1345d787146a1ab1a3e9783a6
parrt <parrt@cs.usfca.edu>
2016-04-22 16:59:51 -0700

    add generic sql

---
+++ corpus/sql/training/dmart_bits.sql
@@ -0,0 +1,119 @@
+SELECT  --SourceServer,
+		Client_ID, SourceDB, [Status], Beta,
+		LoadStageDBStartDate, LoadStageDBEndDate,
+		DATEDIFF(minute,LoadStageDBStartDate, LoadStageDBEndDate) AS StageLoadTime,
+		LoadReportDBStartDate, LoadReportDBEndDate,
+		DATEDIFF(minute,LoadReportDBStartDate, LoadReportDBEndDate) AS ReportLoadTime
+FROM ClientConnection
+--WHERE Beta != '2'
+--WHERE Beta = '1'
+ORDER BY Beta,2
+
+--UPDATE ClientConnection
+--SET Beta = '2'
+--WHERE SourceDB IN ('PADemoDU','RLC','PremierAmerica')
+-------------------------------------------
+--UPDATE ClientConnection
+--SET Beta = '0'
+--WHERE SourceDB IN ('AddisonAve32','Chevron','EDCO','ConstructionLoanCompany','Delta','Dupont','Kern32','MembersMortgage','Suncoast32','Wescom')
+--WHERE Client_ID <= 1025 -- Hutchinson, number 30 when sorted by client_id
+
+--UPDATE ClientConnection
+--SET Beta = '1'
+--WHERE Client_ID IN ( '136')
+-------------------------------------------
+--DECLARE @1DayAgo datetime
+--SET @1DayAgo = GetDate() - 2 
+
+--UPDATE ClientConnection
+--SET LoadStageDBStartDate = @1DayAgo
+--,LoadStageDBEndDate = @1DayAgo
+--,LoadReportDBStartDate = @1DayAgo
+--,LoadReportDBEndDate = @1DayAgo
+--,Status = 4
+--WHERE Beta='1'
+--WHERE SourceDB = 'MembersMortgage'
+----------------------------------------------
+/*
+TRUNCATE TABLE DMartLogging
+*/
+SELECT * FROM DMartLogging
+ORDER BY ErrorDateTime desc
+----------------------------------------------
+SELECT name from sys.databases
+WHERE Name LIKE '%Stage%'
+----------------------------------------------
+SELECT name from sys.databases
+WHERE Name LIKE '%Data%'
+----------------------------------------------
+SELECT * FROM opsinfo_ops_dbo.clients
+WHERE client_name LIKE '%Merrimack%'
+----------------------------------------------
+SELECT  Client_id, SourceServer, SourceDB, Status, Beta, StageServer, StageDB, ReportServer, ReportDB, 
+		LoadStageDBStartDate, LoadStageDBEndDate, 
+		DATEDIFF(minute,LoadStageDBStartDate, LoadStageDBEndDate) AS StageLoadTime,
+		LoadReportDBStartDate, LoadReportDBEndDate,
+		DATEDIFF(minute,LoadReportDBStartDate, LoadReportDBEndDate) AS ReportLoadTime
+FROM ClientConnection
+--WHERE ReportServer = 'PSQLRPT22'
+--WHERE SourceServer = 'STGSQL511'
+--WHERE Beta = 2
+ORDER BY Beta,3
+----------------
+
+----------------------------------------------------------------
+
+----------------
+SELECT  *
+FROM ClientConnection
+WHERE ReportServer = 'PSQLRPT22'
+ORDER BY Beta,3 --DESC
+----------------
+SELECT  Client_ID, SourceServer, SourceDB, StageServer, StageDB, ReportServer, ReportDB, Status, Beta
+FROM ClientConnection
+WHERE ReportServer = 'PSQLRPT22'
+ORDER BY Beta,3 --DESC
+----------------
+
+UPDATE ClientConnection
+SET Status = '0' 
+WHERE Beta = '1' 
+
+
+UPDATE ClientConnection
+SET LoadStageDBStartDate = @Now ,LoadStageDBEndDate = @Now ,Status = 2
+WHERE Beta='1'	
+
+UPDATE ClientConnection
+SET ReportServer = 'PSQLRPT22'
+WHERE ReportServer = 'PSLQRPT22'
+
+---------------------------------------------
+UPDATE ClientConnection
+SET StageServer = 'PSQLRPT22', ReportServer = 'PSQLRPT22', Beta = '0'
+WHERE SourceDB = 'Redwood'
+
+UPDATE ClientConnection
+SET SourceServer = 'PSQLDLS30'
+WHERE SourceDB = 'HiwayCU'
+
+UPDATE ClientConnection
+SET Client_ID = '228'
+WHERE SourceDB = 'RLC'
+
+
+
+SELECT SUM(StageLoadTime) AS TotalStageLoadTime, SUM(ReportLoadTime) AS TotalReportLoadTime
+FROM
+(
+SELECT  --SourceServer,
+		Client_ID, SourceDB, [Status], Beta,
+		LoadStageDBStartDate, LoadStageDBEndDate,
+		DATEDIFF(minute,LoadStageDBStartDate, LoadStageDBEndDate) AS StageLoadTime,
+		LoadReportDBStartDate, LoadReportDBEndDate,
+		DATEDIFF(minute,LoadReportDBStartDate, LoadReportDBEndDate) AS ReportLoadTime
+FROM ClientConnection
+--WHERE Beta != '2'
+--WHERE Beta = '1'
+--ORDER BY Beta,2
+) t
---
+++ corpus/sql/training/ex_SUMbyColumn.sql
@@ -0,0 +1,18 @@
+insert into t values ('AA',    150,     100,        50,        4)
+insert into t values ('HHH',   161,     125,        36,        4)
+insert into t values ('PPPP',  160 ,    85,         75,        4)
+insert into t values ('JJJJJ', 120,     56,         64,        2)
+insert into t values ('GGGG',  40,      31,         9,         2)
+
+SELECT  CASE WHEN Col IS NULL THEN 'Total'
+             ELSE Col
+        END COL
+      , SUM(COL1) AS Col1
+      , SUM(COL2) AS COL2
+      , SUM(COL3) AS COL3
+      , SUM(COL4) AS COL4
+FROM    t
+GROUP BY COL
+ORDER BY CASE WHEN Col IS NULL THEN 999
+              ELSE 0
+         END 
