a728c166741e504cfac85c096821aa0758d9e278
parrt <parrt@cs.usfca.edu>
2016-04-24 13:13:44 -0700

    add antlr4 capture test

--- src/org/antlr/codebuff/Tool.java
+++ src/org/antlr/codebuff/Tool.java
@@ -47,10 +47,12 @@ public class Tool {
 
 	public static final LangDescriptor JAVA_DESCR =
 		new LangDescriptor("java", ".*\\.java", JavaLexer.class, JavaParser.class, "compilationUnit", 4);
+	public static final LangDescriptor ANTLR4_DESCR =
+		new LangDescriptor("antlr", ".*\\.g4", ANTLRv4Lexer.class, ANTLRv4Parser.class, "grammarSpec", 4);
 
 	public static LangDescriptor[] languages = new LangDescriptor[] {
 		JAVA_DESCR,
-		new LangDescriptor("antlr", ".*\\.g4", ANTLRv4Lexer.class, ANTLRv4Parser.class, "grammarSpec", 4),
+		ANTLR4_DESCR,
 		new LangDescriptor("sqlite", ".*\\.sql", SQLiteLexer.class, SQLiteParser.class, "parse", 4),
 		new LangDescriptor("tsql", ".*\\.sql", tsqlLexer.class, tsqlParser.class, "tsql_file", 4),
 		new LangDescriptor("plsql", ".*\\.sql", plsqlLexer.class, plsqlParser.class, "compilation_unit", 4)
--- test/org/antlr/codebuff/BaseTest.java
+++ test/org/antlr/codebuff/BaseTest.java
@@ -1,25 +1,9 @@
 package org.antlr.codebuff;
 
-import org.junit.Test;
-
-import static org.junit.Assert.assertTrue;
-
 public class BaseTest {
 	public String fileName;
 
 	public BaseTest(String fileName) {
 		this.fileName = fileName;
-	}
-
-	@Test
-	public void testCapture() throws Exception {
-		Corpus corpus = new Corpus(fileName, ".*\\.java", Tool.JAVA_DESCR);
-		corpus.train();
-		InputDocument testDoc = Tool.load(fileName, corpus.language);
-		Formatter formatter = new Formatter(corpus);
-		String output = formatter.format(testDoc, false);
-		float editDistance = formatter.getEditDistance();
-		System.out.println("edit distance "+editDistance);
-		assertTrue("WS edit distance too high "+editDistance, editDistance < 0.05);
 	}
 }
