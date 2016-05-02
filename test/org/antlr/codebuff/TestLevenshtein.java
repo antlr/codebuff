e8030d364c0692d9f42ba4068eb8bddd27e23765
parrt <parrt@cs.usfca.edu>
2016-05-02 11:31:47 -0700

    fix unit test

--- test/org/antlr/codebuff/TestLevenshtein.java
+++ test/org/antlr/codebuff/TestLevenshtein.java
@@ -8,12 +8,12 @@ public class TestLevenshtein {
 	@Test
 	public void simple() {
 		float d = Tool.levenshteinDistance("abc", "abd");
-		assertEquals(1, d);
+		assertEquals(1.0/3.0, d, 0.00001);
 
 		d = Tool.levenshteinDistance("kitten", "sitten");
-		assertEquals(1, d);
+		assertEquals(1.0/"kitten".length(), d, 0.00001);
 
 		d = Tool.levenshteinDistance("sittin", "sitting");
-		assertEquals(1, d);
+		assertEquals(1.0/"sitting".length(), d, 0.00001);
 	}
 }
