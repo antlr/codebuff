cd7f2f733412134e8971d5ae0e176aae2fce4e9c
parrt <parrt@cs.usfca.edu>
2016-05-03 15:24:47 -0700

    add todo

--- src/org/antlr/codebuff/Trainer.java
+++ src/org/antlr/codebuff/Trainer.java
@@ -577,7 +577,7 @@ public class Trainer {
 		features[INDEX_PREV_EARLIEST_RIGHT_ANCESTOR]  = rulealt(prevEarliestAncestorRuleIndex,prevEarliestAncestorRuleAltNum);
 		features[INDEX_CUR_TYPE]                      = curToken.getType();
 		features[INDEX_EARLIEST_LEFT_ANCESTOR]        = rulealt(earliestLeftAncestor);
-		features[INDEX_ANCESTORS_CHILD_INDEX]         = getChildIndexOrListMembership(earliestLeftAncestor);
+		features[INDEX_ANCESTORS_CHILD_INDEX]         = getChildIndexOrListMembership(earliestLeftAncestor); // TODO: dup with below!!!
 		features[INDEX_ANCESTORS_PARENT_RULE]         = earliestLeftAncestorParent!=null ? rulealt(earliestLeftAncestorParent) : -1;
 		features[INDEX_ANCESTORS_PARENT_CHILD_INDEX]  = getChildIndexOrListMembership(earliestLeftAncestor);
 		features[INDEX_ANCESTORS_PARENT2_RULE]        = earliestLeftAncestorParent2!=null ? rulealt(earliestLeftAncestorParent2) : -1;
