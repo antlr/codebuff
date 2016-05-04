eaefd4d077cfaf4c2ed187d4e050f8d08783df62
parrt <parrt@cs.usfca.edu>
2016-05-04 09:20:49 -0700

    update python/pdf after changing model

--- python/src/drop_one_align_feature.py
+++ python/src/drop_one_align_feature.py
@@ -1,6 +1,6 @@
 #
 # AUTO-GENERATED FILE. DO NOT EDIT
-# CodeBuff 1.4.2 'Tue May 03 13:15:34 PDT 2016'
+# CodeBuff 1.4.3 'Tue May 03 23:57:06 PDT 2016'
 #
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,13 +11,13 @@ N = 18
 featureIndexes = range(0,N)
 java = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 ax.plot(featureIndexes, java, label="java")
-sqlite = [0.051282052, 0.04761905, 0.04761905, 0.04, 0.051282052, 0.13513513, 0.04347826, 0.054054055, 0.05172414, 0.051282052, 0.04761905, 0.051282052, 0.04, 0.04347826, 0.04761905, 0.05140187, 0.03797468, 0.03846154]
+sqlite = [0.051282052, 0.039215688, 0.05357143, 0.054054055, 0.051282052, 0.14473684, 0.043956045, 0.039215688, 0.051282052, 0.054054055, 0.04029304, 0.03448276, 0.0375, 0.04347826, 0.03846154, 0.02631579, 0.02631579, 0.04029304]
 ax.plot(featureIndexes, sqlite, label="sqlite")
 java8 = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 ax.plot(featureIndexes, java8, label="java8")
 antlr = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.041666668, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 ax.plot(featureIndexes, antlr, label="antlr")
-tsql = [0.054054055, 0.055555556, 0.057692308, 0.048387095, 0.054054055, 0.13461539, 0.054054055, 0.038961038, 0.054054055, 0.054054055, 0.057692308, 0.054054055, 0.057692308, 0.057692308, 0.055555556, 0.048387095, 0.057142857, 0.055555556]
+tsql = [0.058139537, 0.06382979, 0.058139537, 0.06521739, 0.058139537, 0.114285715, 0.05882353, 0.05263158, 0.06666667, 0.061452515, 0.058139537, 0.057142857, 0.0754717, 0.055555556, 0.058139537, 0.0754717, 0.069767445, 0.06382979]
 ax.plot(featureIndexes, tsql, label="tsql")
 
 labels = ['curated', ' LT(-1)', 'LT(-1) right ancestor', ' LT(1)', 'Pair dif\n', 'Strt line', 'Big list', 'List elem.', 'LT(1) left ancestor', 'ancestor child index', ' parent', 'parent child index', ' parent^2', 'parent^2 child index', ' parent^3', 'parent^3 child index', ' parent^4', 'parent^4 child index', 'LT(-1)', 'LT(-1) right ancestor', 'LT(1)', 'Pair dif\n', 'Strt line', 'Big list', 'List elem.', 'LT(1) left ancestor', 'ancestor child index', 'parent', 'parent child index', 'parent^2', 'parent^2 child index', 'parent^3', 'parent^3 child index', 'parent^4', 'parent^4 child index', 'LT(-1)', 'LT(-1) right ancestor', 'LT(1)', 'Pair dif\n', 'Strt line', 'Big list', 'List elem.', 'LT(1) left ancestor', 'ancestor child index', 'parent', 'parent child index', 'parent^2', 'parent^2 child index', 'parent^3', 'parent^3 child index', 'parent^4', 'parent^4 child index', 'LT(-1)', 'LT(-1) right ancestor', 'LT(1)', 'Pair dif\n', 'Strt line', 'Big list', 'List elem.', 'LT(1) left ancestor', 'ancestor child index', 'parent', 'parent child index', 'parent^2', 'parent^2 child index', 'parent^3', 'parent^3 child index', 'parent^4', 'parent^4 child index', 'LT(-1)', 'LT(-1) right ancestor', 'LT(1)', 'Pair dif\n', 'Strt line', 'Big list', 'List elem.', 'LT(1) left ancestor', 'ancestor child index', 'parent', 'parent child index', 'parent^2', 'parent^2 child index', 'parent^3', 'parent^3 child index', 'parent^4', 'parent^4 child index', 'LT(-1)', 'LT(-1) right ancestor', 'LT(1)', 'Pair dif\n', 'Strt line', 'Big list', 'List elem.', 'LT(1) left ancestor', 'ancestor child index', 'parent', 'parent child index', 'parent^2', 'parent^2 child index', 'parent^3', 'parent^3 child index', 'parent^4', 'parent^4 child index']
--- python/src/drop_one_ws_feature.py
+++ python/src/drop_one_ws_feature.py
@@ -1,6 +1,6 @@
 #
 # AUTO-GENERATED FILE. DO NOT EDIT
-# CodeBuff 1.4.2 'Tue May 03 12:48:54 PDT 2016'
+# CodeBuff 1.4.3 'Tue May 03 23:16:10 PDT 2016'
 #
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,15 +9,15 @@ fig = plt.figure()
 ax = plt.subplot(111)
 N = 8
 featureIndexes = range(0,N)
-java = [0.054545455, 0.056737587, 0.054347824, 0.054347824, 0.058132343, 0.04, 0.054545455, 0.05473098]
+java = [0.05541237, 0.06451613, 0.05541237, 0.054545455, 0.05757576, 0.04, 0.05541237, 0.057843138]
 ax.plot(featureIndexes, java, label="java")
-sqlite = [0.09677419, 0.099585064, 0.09128631, 0.0950764, 0.10696095, 0.082417585, 0.10074627, 0.09405941]
+sqlite = [0.10169491, 0.11640212, 0.10152284, 0.107344635, 0.121212125, 0.08571429, 0.10897436, 0.10186757]
 ax.plot(featureIndexes, sqlite, label="sqlite")
-java8 = [0.054545455, 0.056737587, 0.054347824, 0.054347824, 0.058132343, 0.04, 0.054545455, 0.05473098]
+java8 = [0.042056076, 0.04699739, 0.042056076, 0.042056076, 0.042821158, 0.039719626, 0.042056076, 0.042821158]
 ax.plot(featureIndexes, java8, label="java8")
-antlr = [0.28987992, 0.27787307, 0.31389365, 0.28987992, 0.2915952, 0.23021583, 0.2915952, 0.28987992]
+antlr = [0.29331046, 0.27444252, 0.3053173, 0.29331046, 0.29331046, 0.27916196, 0.29331046, 0.29331046]
 ax.plot(featureIndexes, antlr, label="antlr")
-tsql = [0.08713693, 0.08984375, 0.09293681, 0.09071274, 0.097402595, 0.08184143, 0.08713693, 0.08983051]
+tsql = [0.08450704, 0.08203125, 0.0859375, 0.08857143, 0.09859155, 0.077922076, 0.08450704, 0.088607594]
 ax.plot(featureIndexes, tsql, label="tsql")
 
 labels = ['curated', ' LT(-1)', 'Strt line', 'LT(-1) right ancestor', ' LT(1)', 'Big list', 'List elem.', 'LT(1) left ancestor', 'LT(-1)', 'Strt line', 'LT(-1) right ancestor', 'LT(1)', 'Big list', 'List elem.', 'LT(1) left ancestor', 'LT(-1)', 'Strt line', 'LT(-1) right ancestor', 'LT(1)', 'Big list', 'List elem.', 'LT(1) left ancestor', 'LT(-1)', 'Strt line', 'LT(-1) right ancestor', 'LT(1)', 'Big list', 'List elem.', 'LT(1) left ancestor', 'LT(-1)', 'Strt line', 'LT(-1) right ancestor', 'LT(1)', 'Big list', 'List elem.', 'LT(1) left ancestor', 'LT(-1)', 'Strt line', 'LT(-1) right ancestor', 'LT(1)', 'Big list', 'List elem.', 'LT(1) left ancestor']
