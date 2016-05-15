78eb1c976f36a02788450dd678d55a3f5e4da0f6
parrt <parrt@cs.usfca.edu>
2016-05-15 14:23:36 -0700

    add code to compare file vs corpus predictions.

--- src/org/antlr/codebuff/rednaxela_kdtree/DistanceFunction.java
+++
@@ -1,8 +0,0 @@
-package org.antlr.codebuff.rednaxela_kdtree;
-
-public interface DistanceFunction {
-	public double distance(double[] p1, double[] p2);
-
-	public double distanceToRect(double[] point, double[] min, double[] max);
-}
-
